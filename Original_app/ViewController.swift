8d31b934957d2679522239a619a067e9051b3d0f
kannahotta <k@hotta.me>
2022-11-29 20:09:52 +0900

    日付のString型？への変換が難しいです。

--- Original_app/ViewController.swift
+++ Original_app/ViewController.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var number: Int!
+    
+    
+    @IBOutlet var mainbutton: UIButton!
+    
+    
+    @IBOutlet var datelabel: UILabel!
+    
+    
+    @IBOutlet var locatelabel: UILabel!
+    
+
+    
+    @IBAction func SecondView() {
+        
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
+        
+        //メインボタンの角を丸くする
+        mainbutton.layer.cornerRadius = 25
+        mainbutton.clipsToBounds = true
+      
+        //日付ラベルの角を丸くする
+        datelabel.layer.cornerRadius = 25
+        datelabel.clipsToBounds = true
+        
+        //日付を表示させる
+        let date = Date()
+        let dateFormatter = DateFormatter();
+        
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMdHms")
+        
+        print(dataFormatterstring(from: dt))
+        
+        
+        //位置ラベルの角を丸くする
+        locatelabel.layer.cornerRadius = 25
+        locatelabel.clipsToBounds = true
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
