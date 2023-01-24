30b522a11fe8629a9985d3946bd83371c32f3dfd
kannahotta <k@hotta.me>
2023-01-24 20:05:01 +0900

    現在地の座標取得段階ですが、変なところでつっかえてます。

--- Original_app/ViewController.swift
+++ Original_app/ViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
     
@@ -16,18 +17,17 @@ class ViewController: UIViewController {
     
     @IBOutlet var datelabel: UILabel!
     
-    
+    var cityName: String!
     @IBOutlet var locatelabel: UILabel!
     
 
     
     @IBAction func SecondView() {
         
-        
-        
-        
+
     }
     
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         
@@ -41,12 +41,16 @@ class ViewController: UIViewController {
         
         //日付を表示させる
         let date = Date()
-        let dateFormatter = DateFormatter();
+        print(date)
+        let dateFormatter = DateFormatter()
         
-        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMdHms")
         
-        print(dataFormatterstring(from: dt))
+        dateFormatter.dateFormat = "MM月dd日"
         
+        print(dateFormatter.string(from: date))
+        
+        datelabel.text = dateFormatter.string(from: date)
+     
         
         //位置ラベルの角を丸くする
         locatelabel.layer.cornerRadius = 25
@@ -54,8 +58,74 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      
+        
+        
+        
+       // locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+//        //アプリの使用中のみ許可
+        locationManager.requestWhenInUseAuthorization()
+//
+//        //位置情報の取得精度を指定する
+//        locationManager!.desiredAccuacy = CLLocationAccuracyBest
+//
+//        //更新に必要な最小移動距離
+//        //Int値を指定することで、〇〇m間隔で取得するようになる
+//        locationManager!.distanseFilter = 10
+//
+//        //位置情報取得開始
+        locationManager.startUpdatingLocation()
+//
+//        locatelabel.text =
+        
+        
+        
     }
-
-
-}
+    
+    //map許可
+        func locationManager(_ manager: CLLocationManager,didChangeAuthorization status: CLAuthorizationStatus) {// 許可を求めるためのdelegateメソッド
+            switch status {
+            case .notDetermined:// 許可されてない場合
+                manager.requestWhenInUseAuthorization()// 許可を求める
+            case .restricted, .denied:// 拒否されてる場合
+                break// 何もしない
+                
+            case .authorizedWhenInUse: // 許可されている場合
+                manager.startUpdatingLocation()// 現在地の取得を開始
+                
+                //市町村を取得
+                CLGeocoder().reverseGeocodeLocation(locationManager.location!) { placemarks, error in
+                    guard
+                        let placemark = placemarks?.first, error == nil,
+                        let locality = placemark.locality
+                    else {
+                        
+                        return
+                    }
+                    self.cityName = locality
+                    self.locatelabel.text = self.cityName!
+                    
+                }
+                break
+            default:
+                break
+            }
+        }
+        
+        /* 位置情報取得失敗時に実行される関数 */
+        func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+            // この例ではLogにErrorと表示するだけ．
+            //アラート　位置情報をオンにしてください。って出す
+            NSLog("Error")
+        }
+    
+    //座標を取得する
+    
+    func manager.startUpdatingLocation()// 現在地の取得を開始
+            
+    
+        }
 
